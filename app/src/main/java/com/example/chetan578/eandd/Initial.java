09c6bbeb486be58e5d7371e615d4206d2a259471
chetan sobti <sobti.chetan.2009@gmail.com>
2019-04-23 14:11:12 +0530

    recent

---
+++ app/src/main/java/com/example/chetan578/eandd/Initial.java
@@ -0,0 +1,13 @@
+package com.example.chetan578.eandd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Initial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+    }
+}
